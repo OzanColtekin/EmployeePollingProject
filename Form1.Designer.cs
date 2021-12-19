7708a6deddf536d56848396d7e6a21dab3fd19d4
OzanColtekin <ozan.coltekin@outlook.com>
2021-12-19 14:10:10 +0300

    PollingProject

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace EgeYapı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.isciList = new System.Windows.Forms.DataGridView();
+            this.Gelenler = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.isciList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // isciList
+            // 
+            this.isciList.AllowUserToAddRows = false;
+            this.isciList.AllowUserToDeleteRows = false;
+            this.isciList.AllowUserToOrderColumns = true;
+            this.isciList.AllowUserToResizeColumns = false;
+            this.isciList.AllowUserToResizeRows = false;
+            this.isciList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.isciList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.isciList.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.isciList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.isciList.Location = new System.Drawing.Point(12, 12);
+            this.isciList.MultiSelect = false;
+            this.isciList.Name = "isciList";
+            this.isciList.ReadOnly = true;
+            this.isciList.RowHeadersWidth = 51;
+            this.isciList.RowTemplate.Height = 24;
+            this.isciList.Size = new System.Drawing.Size(776, 198);
+            this.isciList.TabIndex = 0;
+            this.isciList.DoubleClick += new System.EventHandler(this.isciList_DoubleClick);
+            this.isciList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.isciList_KeyPress);
+            // 
+            // Gelenler
+            // 
+            this.Gelenler.FormattingEnabled = true;
+            this.Gelenler.ItemHeight = 16;
+            this.Gelenler.Location = new System.Drawing.Point(12, 262);
+            this.Gelenler.Name = "Gelenler";
+            this.Gelenler.Size = new System.Drawing.Size(184, 244);
+            this.Gelenler.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kaydedilecek İsimler";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(469, 229);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(361, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İsim Ara -";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(502, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Metin Belgesine Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(299, 463);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 43);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(202, 463);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(502, 414);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(286, 43);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Excel\'e Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 518);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Gelenler);
+            this.Controls.Add(this.isciList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ege Yapı Ray";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.isciList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView isciList;
+        private System.Windows.Forms.ListBox Gelenler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace EgeYapı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isciList.ColumnCount = 1;
+            isciList.Columns[0].Name = "Adı Soyadı";
+            isciList.Columns[0].Width = 450;
+            ListMembers();
+        }
+
+        public void ListMembers()
+        {
+            string path = Application.StartupPath;
+            string[] kisiler = System.IO.File.ReadAllLines(path + @"\isimler.txt");
+            foreach (string kisi in kisiler)
+            {
+                isciList.Rows.Add(kisi);
+            }
+
+        }
+        public void ListMembersWithName(string text)
+        {
+            int x = 1;
+            string path = Application.StartupPath;
+            string[] kisiler = System.IO.File.ReadAllLines(path + @"\isimler.txt");
+            foreach (string kisi in kisiler)
+            {
+                string lower = kisi.ToLower();
+                if (lower.StartsWith(text))
+                {
+                    isciList.Rows.Add(kisi);
+                    x++;
+                }
+                
+            }
+        }
+
+        private void isciList_DoubleClick(object sender, EventArgs e)
+        {
+
+            string seciliKisi = isciList.CurrentRow.Cells[0].Value.ToString();
+            int _indexOf = Gelenler.Items.IndexOf(seciliKisi);
+            if(_indexOf == -1)
+            {
+                Gelenler.Items.Add(seciliKisi);
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                isciList.Rows.Clear();
+                ListMembers();
+            }
+            else
+            {
+                string kisi = textBox1.Text.ToLower();
+                isciList.Rows.Clear();
+                ListMembersWithName(kisi);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Gelenler.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(Gelenler.SelectedItem == null)
+            {
+                MessageBox.Show("Silmek istediğiniz isimi yan taraftan seçin.");
+            }
+            else
+            {
+                Gelenler.Items.Remove(Gelenler.SelectedItem);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Gelenler.Items.Count == 0) { 
+                MessageBox.Show("Kaydedilecek isimler seçili değil."); 
+            }
+            else
+            {
+            
+            string _Date = DateTime.Now.ToString("d-M-yyyy");
+            string path = Application.StartupPath + @"\Puantaj Kayıtları\" + _Date + " Gelenler.txt";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path));
+            }
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                
+                for(int i =0; i<Gelenler.Items.Count; i++)
+                {
+                    sw.WriteLine(Gelenler.Items[i]);
+                }
+            }
+            }
+            MessageBox.Show("Metin Belgesi dosyası kaydedildi.");
+        }
+
+        private void Sayfa_Ayarla(Excel.Worksheet Sayfa1, string tam_tarih)
+        {
+
+            Excel.Range A1 = (Excel.Range)Sayfa1.Cells[1, 1];
+            A1.Value = tam_tarih + @" MARMARAY CR3 KAPSAMINDA YAPIMI DEVAM EDEN HAYDAPAŞA GAR SAHASI ARKELOJİ KAZI ÇALIŞMASINDA ÇALIŞAN PERSONEL LİSTESİ";
+            A1.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            A1.Font.Size = 18;
+            A1.Font.Bold = true;
+            ((Excel.Range)Sayfa1.Range[(Excel.Range)Sayfa1.Cells[1, 1], (Excel.Range)Sayfa1.Cells[1, 6]]).Cells.Merge();
+            A1.WrapText = true;
+            A1.ColumnWidth = 8.11;
+            A1.RowHeight = 51;
+            A1.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            A1.VerticalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            A1.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+
+            Excel.Range A2 = (Excel.Range)Sayfa1.Cells[2, 1];
+            A2.Interior.Color = Excel.XlRgbColor.rgbYellow;
+
+            Excel.Range B2 = (Excel.Range)Sayfa1.Cells[2, 2];
+            B2.Value = "ADI SOYADI";
+            B2.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            B2.Font.Size = 12;
+            B2.ColumnWidth = 24.56;
+            B2.Font.Bold = true;
+            B2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            Excel.Range C2 = (Excel.Range)Sayfa1.Cells[2, 3];
+            C2.Value = "SIRA NO";
+            C2.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            C2.Font.Size = 12;
+            C2.ColumnWidth = 8.11;
+            C2.Font.Bold = true;
+            C2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            Excel.Range D2 = (Excel.Range)Sayfa1.Cells[2, 4];
+            D2.Value = "ADI SOYADI";
+            D2.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            D2.Font.Size = 12;
+            D2.ColumnWidth = 11.56;
+            D2.Font.Bold = true;
+            D2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            Excel.Range E2 = (Excel.Range)Sayfa1.Cells[2, 5];
+            E2.Value = "SIRA NO";
+            E2.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            E2.Font.Size = 12;
+            E2.ColumnWidth = 8.11;
+            E2.Font.Bold = true;
+            E2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            Excel.Range F2 = (Excel.Range)Sayfa1.Cells[2, 6];
+            F2.Value = "ADI SOYADI";
+            F2.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            F2.Font.Size = 12;
+            F2.ColumnWidth = 47.67;
+            F2.Font.Bold = true;
+            F2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            Excel.Range F3 = (Excel.Range)Sayfa1.Cells[3, 6];
+            F3.Value = "İZİNLİ OLAN PERSONEL LİSTESİ";
+            F3.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            F3.Font.Size = 12;
+            F3.Font.Bold = true;
+            F3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+            Excel.Range F5 = (Excel.Range)Sayfa1.Cells[5, 6];
+            F5.Value = "İŞE GELMEYEN PERSONEL LİSTESİ";
+            F5.Interior.Color = Excel.XlRgbColor.rgbYellow;
+            F5.Font.Size = 12;
+            F5.Font.Bold = true;
+            F5.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string tam_tarih = DateTime.Now.ToString("d");
+            string[] gün_ay = DateTime.Now.ToString("d M").Split(' ');
+            string ay   = DateTime.Now.ToString("MMMM").ToUpper();
+            string yıl  = DateTime.Now.ToString("yyyy");
+            string file_path = Application.StartupPath + @"\Puantaj Kayıtları\" + ay + " AYI PUANTAJ ( " + yıl + " ).xlsx";
+            Excel.Application uygulama = new Excel.Application();
+            uygulama.Visible = true;
+
+            int a = 1;
+            int b = 3;
+
+            if (!File.Exists(file_path))
+            {
+
+                Excel.Workbook kitap = uygulama.Workbooks.Add(System.Reflection.Missing.Value);
+                Excel.Worksheet Sayfa1 = (Excel.Worksheet)kitap.Sheets[1];
+                Sayfa1.Name = "1";
+
+                Sayfa_Ayarla(Sayfa1, tam_tarih);
+
+                
+
+                for (int i = 0; i <Gelenler.Items.Count; i++)
+                {
+                    bool failed = false;
+                    do
+                    {
+                        try
+                        {
+                            Excel.Range alan = (Excel.Range)Sayfa1.Cells[b, 2];
+                            Excel.Range alan2 = (Excel.Range)Sayfa1.Cells[b, 1];
+                            Excel.Range C3 = (Excel.Range)Sayfa1.Cells[b, 3];
+                            Excel.Range D3 = (Excel.Range)Sayfa1.Cells[b, 4];
+                            Excel.Range E3 = (Excel.Range)Sayfa1.Cells[b, 5];
+                            Excel.Range F3 = (Excel.Range)Sayfa1.Cells[b, 6];
+                            C3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            D3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            E3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            F3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            alan.Value = Gelenler.Items[i].ToString().ToUpper();
+                            alan.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            alan2.Value = a;
+                            alan2.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                            alan2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            b++;
+                            a++;
+                            failed = false;
+                        }
+                        catch (System.Runtime.InteropServices.COMException err)
+                        {
+                            failed = true;
+                        }
+                        System.Threading.Thread.Sleep(10);
+                    } while (failed);
+                    
+                }
+            }
+            else
+            {
+                Excel.Workbook kitap = uygulama.Workbooks.Open(file_path);
+                Excel.Worksheet Sayfa1 = (Excel.Worksheet)kitap.Sheets.Add();
+                Sayfa1.Name = gün_ay[0];
+
+                Sayfa_Ayarla(Sayfa1, tam_tarih);
+
+                for (int i = 0; i < Gelenler.Items.Count; i++)
+                {
+                    bool failed = false;
+                    do
+                    {
+                        try
+                        {
+                            Excel.Range alan = (Excel.Range)Sayfa1.Cells[b, 2];
+                            Excel.Range alan2 = (Excel.Range)Sayfa1.Cells[b, 1];
+                            Excel.Range C3 = (Excel.Range)Sayfa1.Cells[b, 3];
+                            Excel.Range D3 = (Excel.Range)Sayfa1.Cells[b, 4];
+                            Excel.Range E3 = (Excel.Range)Sayfa1.Cells[b, 5];
+                            Excel.Range F3 = (Excel.Range)Sayfa1.Cells[b, 6];
+                            C3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            D3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            E3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            F3.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            alan.Value = Gelenler.Items[i].ToString().ToUpper();
+                            alan.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            alan2.Value = a;
+                            alan2.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                            alan2.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                            b++;
+                            a++;
+                            failed = false;
+                        }
+                        catch (System.Runtime.InteropServices.COMException err)
+                        {
+                            failed = true;
+                        }
+                        System.Threading.Thread.Sleep(10);
+                    } while (failed);
+                    
+                }
+
+            }
+            
+
+            
+        }
+
+        private void isciList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar ==(char) Keys.Enter)
+            {
+                int i = isciList.CurrentRow.Index;
+                if(i != 0)
+                {
+                    string seciliKisi = isciList.Rows[i - 1].Cells[0].Value.ToString();
+                    int _indexOf = Gelenler.Items.IndexOf(seciliKisi);
+                    if (_indexOf == -1)
+                    {
+                        Gelenler.Items.Add(seciliKisi);
+                    }
+                }
+                else
+                {
+                    string seciliKisi = isciList.CurrentRow.Cells[0].Value.ToString();
+                    int _indexOf = Gelenler.Items.IndexOf(seciliKisi);
+                    if (_indexOf == -1)
+                    {
+                        Gelenler.Items.Add(seciliKisi);
+                    }
+                }
+
+            }
+        }
+    }
+}
